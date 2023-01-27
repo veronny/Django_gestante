5f590eebe397588701a5028ad44017d0694fd216
Veronny Jhony Morales Castillo <veronny393@hotmail.com>
2023-01-27 15:18:39 -0500

    genera modelos principales

--- filiacion/admin.py
+++ filiacion/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
-from .models import Filiacion
+from .models import Filiacion, Red, Microred, Establecimiento, Provincia, Distrito
 
 # Register your models here.
 
 admin.site.register(Filiacion)
+admin.site.register(Red)
+admin.site.register(Microred)
+admin.site.register(Establecimiento)
+admin.site.register(Provincia)
+admin.site.register(Distrito)
--- filiacion/models.py
+++ filiacion/models.py
@@ -1,5 +1,35 @@
 from django.db import models
 
+class Red(models.Model):
+    nombre_red = models.CharField(max_length=100, default="", null=True, blank=True)
+    cod_red = models.CharField(max_length=10, default="", null=True, blank=True)
+    def __str__(self):
+        return self.nombre_red   
+class Microred(models.Model):
+    nombre_microred = models.CharField(max_length=100,null=True, blank=True)
+    cod_mic = models.CharField(max_length=10, default="",null=True, blank=True)
+    cod_red = models.CharField(max_length=10, default="",null=True, blank=True)
+    red = models.ForeignKey(Red, on_delete=models.CASCADE, related_name='redes',null=True, blank=True)  
+    def __str__(self):
+        return self.nombre_microred   
+class Establecimiento(models.Model):
+    nombre_establecimiento = models.CharField(max_length=100, null=True, blank=True)
+    codigo_unico = models.CharField(max_length=100, default="", null=True, blank=True)
+    red = models.ForeignKey(Red, on_delete=models.CASCADE, null=True, blank=True)
+    microred = models.ForeignKey(Microred, on_delete=models.CASCADE, related_name='microredes', null=True, blank=True)  
+    def __str__(self):
+        return self.nombre_establecimiento     
+class Provincia(models.Model):
+    nombre_provincia = models.CharField(max_length=100,null=True, blank=True)
+    ubigeo = models.CharField(max_length=100, null=True, blank=True)
+    def __str__(self):
+        return self.nombre_provincia        
+class Distrito(models.Model):
+    nombre_distrito = models.CharField(max_length=100, null=True, blank=True)
+    ubigeo = models.CharField(max_length=100, null=True, blank=True)
+    provincia = models.ForeignKey(Provincia, on_delete=models.CASCADE, related_name='provincias', null=True, blank=True)  
+    def __str__(self):
+        return self.nombre_distrito
 # Create your models here.
 class Filiacion(models.Model):
     provincia = models.CharField(max_length=20,null=True, blank=True)
